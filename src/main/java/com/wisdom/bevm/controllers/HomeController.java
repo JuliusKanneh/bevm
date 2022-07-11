3311ff4f414e421e56d831dca057347c4c5ea3f2
JuliusKanneh <juliuskanneh7@gmail.com>
2022-07-11 18:29:27 +0200

    UI integrated successfully

--- src/main/java/com/wisdom/bevm/controllers/HomeController.java
+++ src/main/java/com/wisdom/bevm/controllers/HomeController.java
@@ -1,13 +1,19 @@
 package com.wisdom.bevm.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        return "<h2>Hello</h2>";
+        return "index";
+    }
+
+    @GetMapping("/layout")
+    public String layout(){
+        return "_layout";
     }
 }
